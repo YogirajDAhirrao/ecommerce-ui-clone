56a03f6523e466404bb88321c9bc1aa21ae0ab5f
Yogiraj <yogirajp384@gmail.com>
2024-08-14 19:53:16 +0530

    Make Payment Summary Interactive

--- script/Checkout/orderSummary.js
+++ script/Checkout/orderSummary.js
@@ -3,6 +3,7 @@ import { products ,getProduct} from '../../data/products.js';
 import { formatCurrency } from '../utils/money.js';
 import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
 import { deliveryOptions, getDeliveryOptions } from '../../data/deliveryOptions.js';
+import { renderPaymentSummary } from './paymentSummary.js';
 
 
 let s = dayjs();
@@ -100,6 +101,7 @@ export  function renderOrderSummary(){
       
       const container = document.querySelector(`.js-cart-item-container-${productId}`);
       container.remove();
+      renderPaymentSummary();
 
     });
   });
@@ -110,6 +112,7 @@ export  function renderOrderSummary(){
       const deliveryOptionId = element.dataset.deliveryOptionId;
       updateDeliveryOptions(productId, deliveryOptionId);
       renderOrderSummary();
+      renderPaymentSummary();
 
     });
   });
